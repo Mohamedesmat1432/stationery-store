d1fff5399bf8cface26bc31497736a2559142c39
Mohamedesmat1432 <Mohamedesmat1432@gmail.com>
2026-04-28 14:27:50 +0300

    install boost + spatie permission + ulid

---
+++ app/Models/Permission.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
+use Spatie\Permission\Models\Permission as SpatiePermission;
+
+class Permission extends SpatiePermission
+{
+    use HasUlids;
+    /**
+     * Primary key is ULID string, not integer
+     */
+    public $incrementing = false;
+    protected $keyType = 'string';
+}
